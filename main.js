bcaaa00b7ee06da55b69eb91980928d1ce3e8513
Pedro Coppola <89110983+PedroHenriqueCoppola@users.noreply.github.com>
2024-09-22 22:38:59 -0300

    taking products name to home select

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+const products = getObjectFromLocalStorage("products");
+
 const addProductForm = document.getElementById("addProductForm");
+const productSelect = document.getElementById("productSelect");
 const productAmount = document.getElementById("productAmount");
 
 addProductForm.addEventListener("submit", e => {
@@ -6,3 +9,12 @@ addProductForm.addEventListener("submit", e => {
 
  })
 
+if (products) {
+    products.forEach(el => {
+        const option = document.createElement("option");
+        option.value = el.code;
+        option.textContent = el.name;
+
+        productSelect.append(option);
+    });
+}
